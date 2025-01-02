4823ed54223f6443ba0dfdd520e3cdea5f053374
Vishal Singh <singh287686@gmail.com>
2025-01-02 23:39:21 +0530

    created the frontend of the project(Home, result, buy credit and login pages and then made the commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext()
+
+const AppContextProvider = (props)=>{
+
+    const [user, setUser] = useState(null);
+    const [showLogin, setShowLogin] = useState(false);
+
+
+    const value = {
+        user, setUser, showLogin, setShowLogin
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider;
