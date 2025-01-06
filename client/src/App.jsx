f7138f7a5cbe19e9ef81d883adb2dab1bd111d09
Vishal Singh <singh287686@gmail.com>
2025-01-06 23:45:40 +0530

    commit changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import { AppContext } from './context/AppContext'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
@@ -14,8 +16,9 @@ const App = () => {
 
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer  position='bottom-right'/>
       <Navbar/>
-      {showLogin && <Login/>}
+      {showLogin && <Login />}
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/result' element={<Result />}/>
